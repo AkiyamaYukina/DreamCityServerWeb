ef00c0e8ff815272ceb8c6d7cf1c966f61c642b8
张海洋 <zhanghaiyang@sh-jmzn.com>
2022-10-14 18:09:50 +0800

    1014初始化提交

---
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public void onPlayerLogin(@RequestParam("username")String username, @RequestParam("password")String password)
+    {
+
+    }
+}
